b0f9da745ca37f6d343cf28e7c201fc3cf485c83
Teri Chudějová <tereza.chudejova@gmail.com>
2026-06-07 23:10:12 +0200

    fix deployment issue

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/teri-a-tom/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/teri-a-tom/' : '/',
   plugins: [
     tailwindcss(),
     react(),
   ],
-})
+}))
